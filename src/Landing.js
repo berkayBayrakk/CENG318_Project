f8c3989afccf517f0e033bb7d6a9160195b45113
Berkay Bayrak <berkay@Berkays-MacBook-Air.local>
2023-04-30 21:59:54 +0300

    pagination update

--- src/Landing.js
+++ src/Landing.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import './Navbar.css';
 import Item from './Item';
 import Avatar from './images/avatar.png';
+import Left from './images/left_pagination.png';
+import Right from './images/right_pagination.png'
 import { itemList } from './mock_db';
 
 const Navbar = () => {
@@ -54,27 +56,7 @@ const Navbar = () => {
     );
   };
 
-const Pagination = ({ currentPage, onPageChange, pageCount }) => {
-  const pageNumbers = Array.from({ length: pageCount }, (_, i) => i + 1);
 
-  const handlePageClick = (pageNumber) => {
-    onPageChange(pageNumber);
-  };
-
-  return (
-    <ul className="pagination">
-      {pageNumbers.map((pageNumber) => (
-        <li
-          key={pageNumber}
-          className={pageNumber === currentPage ? 'active' : ''}
-          onClick={() => handlePageClick(pageNumber)}
-        >
-          {pageNumber}
-        </li>
-      ))}
-    </ul>
-  );
-};
 
 const ItemList = ({ items, currentPage, itemsPerPage }) => {
   const startIndex = (currentPage - 1) * itemsPerPage;
@@ -92,34 +74,70 @@ const ItemList = ({ items, currentPage, itemsPerPage }) => {
   );
 };
 
+const Pagination = ({ Direction, onPageChange, pageCount, currentPage,itemLength }) => {
+  const style=(Direction==='Left')?
+  {display:((currentPage===1 && Direction==='Left')||(currentPage===itemLength && Direction==='Right'))?'none':'flex',width:100,height:100 ,position:'absolute',
+  left:0
+  }:{display:((currentPage===1 && Direction==='Left')||(currentPage===itemLength && Direction==='Right'))?'none':'flex',width:100,height:100 ,position:'absolute',
+  right:0}
+  const pageNumbers = Array.from({ length: pageCount}, (_, i) => i + 1);
+
+  const handlePageClick = (pageNumber) => {
+    onPageChange(pageNumber);
+  };
+
+  return (
+    <div style={{width:100 ,height:100}} onClick={()=>{
+      if(Direction==='Left'){
+        handlePageClick(currentPage-1);
+      }
+      else{
+        handlePageClick(currentPage+1);
+
+      }
+    }}>
+    <img alt='Pagination' src={(Direction==='Left')?Left:Right} 
+   
+    style={style}></img>
+    </div>
+  );
+};
+
 const App = () => {
   const [items, setItems] = useState([]); // array of items
   const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 5;
+  const itemsPerPage = 4;
 
   const handlePageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
   };
   useEffect(()=>{
     setItems(itemList);
-    console.log(12);
   },[])
   const pageCount = Math.ceil(items.length / itemsPerPage);
 
   return (
     <div className="app">
       <Navbar />
-      <div className="content">
-        <div className="sidebar">
+      <div className="content" style={{display:'flex', flexDirection:'column',height:'80%', justifyContent:'center'}}>
+        
+      <div className="sidebar" style={{display:'flex', flexDirection:'row',alignItems:'center',justifyContent:'center'}}>
+          <Pagination Direction={'Left'} currentPage={currentPage} onPageChange={handlePageChange} pageCount={pageCount} itemLength={pageCount} />
+
           <ItemList items={items} currentPage={currentPage} itemsPerPage={itemsPerPage} />
-          <Pagination currentPage={currentPage} onPageChange={handlePageChange} pageCount={pageCount} />
+          <Pagination Direction={'Right'} currentPage={currentPage} onPageChange={handlePageChange} pageCount={pageCount} itemLength={pageCount} />
+
         </div>
-        <div className="sidebar">
-      <ItemList items={items} currentPage={currentPage} itemsPerPage={itemsPerPage} />
-      <Pagination currentPage={currentPage} onPageChange={handlePageChange} pageCount={pageCount} />
-    </div>
+        <hr style={{width:'85%'}}></hr>
+        <div className="sidebar" style={{display:'flex', flexDirection:'row',alignItems:'center',justifyContent:'center'}}>
+        <Pagination Direction={'Left'} currentPage={currentPage} onPageChange={handlePageChange} pageCount={pageCount} itemLength={pageCount}/>
+
+        <ItemList items={items} currentPage={currentPage} itemsPerPage={itemsPerPage} />
+        <Pagination Direction={'Right'} currentPage={currentPage} onPageChange={handlePageChange} pageCount={pageCount} itemLength={pageCount}/>
+       </div>
+
+      </div>
   </div>
-</div>
 );
 };
 export default App;
