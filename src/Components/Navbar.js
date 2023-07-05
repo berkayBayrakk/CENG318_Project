1a7ac0943e43052d03aa2392aa62782de57f5022
enisozgun <enis.ozgun@hotmail.com>
2023-07-05 19:56:55 +0300

    Adjusted Navbar and CreatePoll CSS.

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,69 +1,65 @@
-import Avatar from '../images/avatar.png';
-import { Link, useNavigate } from 'react-router-dom';
-import React, { useEffect,useContext, useState } from 'react';
-import {CredentialContext} from '../Providers/Credentials';
-import Logo from '../images/votio_logo_white.png';
-import './Navbar.css';
+import Avatar from "../images/avatar.png";
+import { Link, useNavigate } from "react-router-dom";
+import React, { useEffect, useContext, useState } from "react";
+import { CredentialContext } from "../Providers/Credentials";
+import Logo from "../images/votio_logo_white.png";
+import "./Navbar.css";
 
 export const Navbar = (props) => {
+  const { logoutFunction, userCredentials } = useContext(CredentialContext);
 
+  const navigation = useNavigate();
 
-    const { logoutFunction, userCredentials } = useContext(CredentialContext);
-
-    const navigation=useNavigate();
-
-    const handleSearch = (event) => {
-      props.setSearchQuery(event.target.value);
-    };
-
-  
-    const handleCreate = () => {
-      navigation('/create-poll');
-    };
-  
-    const handleHelp = () => {
-      navigation('/help');
-      // Handle help menu item click
-    };
-  
-    const handleLogout = () => {
-      logoutFunction();
-       navigation('/'); 
-    };
-
-    useEffect(()=>{
-      //TODO change after css changes
-      if(!userCredentials.isLogged){
-        navigation('/');
-      }
-    },[])
-  
-    return (
-      <nav className="navbar">
-        <div className="navbar-left" onClick={()=>{navigation('/main')}}>
-          <img src={Logo} alt="Logo" className="navbar-logo" />
-        </div>
-        <div className="navbar-search">
-            <input type="text" placeholder="Search Poll" value={props.searchQuery} onChange={handleSearch} />
-        </div>
-        <div className="navbar-right">
-          <button className="navbar-create" onClick={handleCreate}>
-            Create a New Poll
-          </button>
-          <div className="navbar-avatar">
-            <img src={Avatar} alt="Avatar" className="navbar-avatar-img" />
-            <div className="navbar-avatar-menu">
-              <ul style={{listStyleType:'none'}}>
-              <li >{userCredentials.email}</li>
-                <li onClick={handleHelp}>Help</li>
-                <li onClick={handleLogout}>Logout</li>
-
-              </ul>
-            </div>
-          </div>
-        </div>
-      </nav>
-    );
+  const handleSearch = (event) => {
+    props.setSearchQuery(event.target.value);
   };
 
-  
+  const handleCreate = () => {
+    navigation("/create-poll");
+  };
+
+  const handleHelp = () => {
+    navigation("/help");
+    // Handle help menu item click
+  };
+
+  const handleLogout = () => {
+    logoutFunction();
+    navigation("/");
+  };
+
+  useEffect(() => {
+    //TODO change after css changes
+    if (!userCredentials.isLogged) {
+      navigation("/");
+    }
+  }, []);
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-left">
+    <div className="navbar-logo-container" onClick={() => { navigation('/main') }}>
+        <img src={Logo} alt="Logo" className="navbar-logo" />
+    </div>
+    <div className="navbar-search">
+        <input type="text" placeholder="Search Poll" value={props.searchQuery} onChange={handleSearch} />
+    </div>
+</div>
+      <div className="navbar-right">
+        <button className="navbar-create" onClick={handleCreate}>
+          Create a New Poll
+        </button>
+        <div className="navbar-avatar">
+          <img src={Avatar} alt="Avatar" className="navbar-avatar-img" />
+          <div className="navbar-avatar-menu">
+            <ul style={{ listStyleType: "none" }}>
+              <li>{userCredentials.email}</li>
+              <li onClick={handleHelp}>Help</li>
+              <li onClick={handleLogout}>Logout</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
