ced2640967deafbe16f795b8603d83386a772acc
Zeynep <zeybektas35@gmail.com>
2023-07-06 12:31:41 +0300

    help page improved

--- src/Pages/Help.js
+++ src/Pages/Help.js
@@ -5,18 +5,20 @@ export default function HelpPage(){
     return (
         <div className="app">
           <Navbar />
-          <h1>
-          Introduction
-          </h1>
-          <h3>
-          Create polls about literally anything: sport, music, art, a daily topic, or whatever you want. Share your poll with your friends easily via a shortened URL. See the results of the polls and share them. You are never going to be caught. between a rock and a hard place ever again. Let the other ones choose it.
-          </h3>
-          <h1>
-            What is votio
-          </h1>
-          <h4>
-          Votio is a web-based application that aims to simplify decision-making processes through quick and easy-to-use polls. The project will utilize modern web development techniques, including user interface design principles and development patterns and methodologies such as ’AGILE’. Votio will provide users with the ability to create custom polls on a wide range of topics and share them with friends through shortened URLs. The significance of this project lies in its ability to streamline group decision-making processes and eliminate the difficulties associated with reaching a consensus. By providing an efficient and user-friendly polling platform, Votio will generate valuable data (and later can be searched on) that can be used to gain insights into user preferences and behavior. The expected outcome of this project is a fully functional polling platform with an intuitive interface and robust software architecture. Through the creation of Votio, we hope to demonstrate the potential of web technologies to simplify complex decision-making processes and improve communication among individuals.
-          </h4>
+          <div className="help-container" style={{margin: "40px"}}>
+            <h1>
+            Introduction
+            </h1>
+            <h3>
+            Create polls about literally anything: sport, music, art, a daily topic, or whatever you want. Share your poll with your friends easily via a shortened URL. See the results of the polls and share them. You are never going to be caught. between a rock and a hard place ever again. Let the other ones choose it.
+            </h3>
+            <h1 style={{margin: "60px 0 0"}}>
+              What is votio
+            </h1>
+            <h4>
+            Votio is a web-based application that aims to simplify decision-making processes through quick and easy-to-use polls. The project will utilize modern web development techniques, including user interface design principles and development patterns and methodologies such as ’AGILE’. Votio will provide users with the ability to create custom polls on a wide range of topics and share them with friends through shortened URLs. The significance of this project lies in its ability to streamline group decision-making processes and eliminate the difficulties associated with reaching a consensus. By providing an efficient and user-friendly polling platform, Votio will generate valuable data (and later can be searched on) that can be used to gain insights into user preferences and behavior. The expected outcome of this project is a fully functional polling platform with an intuitive interface and robust software architecture. Through the creation of Votio, we hope to demonstrate the potential of web technologies to simplify complex decision-making processes and improve communication among individuals.
+            </h4>
+          </div>
       </div>
     );
 }
